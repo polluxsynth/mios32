119f06987f679924b7244950c1e57158fea811c8
Ricard Wanderlof <ricard2013@butoba.net>
2017-07-11 01:00:59 +0200

    Initialize I2C driver

--- apps/synthesizers/goom-p6/src/app.c
+++ apps/synthesizers/goom-p6/src/app.c
@@ -11,6 +11,7 @@
 
 #include <mios32.h>
 #include "app.h"
+#include "eeprom.h"
 #include "synth.h"
 #include "param.h"
 
@@ -43,6 +44,9 @@ void APP_Init(void)
   // initialize all LEDs
   MIOS32_BOARD_LED_Init(0xffffffff);
 
+  // init EEPROM driver
+  EEPROM_Init(0);
+
   // init P6 I/O scan
   MIOS32_BOARD_P6_ScanInit();
 
@@ -72,6 +76,10 @@ void APP_Background(void)
     // actually refers to. Otherwise, pin_changed might have changed status
     // between the calculation of pin_changed above and its usage here.
     PARAM_ButtonHandle(old_pin_status & 0x1ff);
+
+//  u32 timestamp = MIOS32_TIMESTAMP_Get();
+//  if ((timestamp & 0x3ff) == 0)
+//MIOS32_MIDI_SendDebugMessage("%s: SR2: %04x (%sBUSY)\n", __func__, I2C1->SR2, I2C1->SR2 & 0002 ? "" : "NOT ");
 }
 
 
