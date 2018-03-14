75aa4275c5b7e9935e7e9fd0fe2f5a12c06623dd
Ricard Wanderlof <ricard2013@butoba.net>
2018-03-14 14:03:36 +0100

    Add settable split point: pgm num 1 then 2 then split point on kbd

--- apps/processing/midiproc/src/proc.c
+++ apps/processing/midiproc/src/proc.c
@@ -36,11 +36,16 @@
 // Local definitions
 /////////////////////////////////////////////////////////////////////////////
 
+enum proc_sm { IDLE, ARMED, SPLIT_SET };
+
+#define SPLIT_MAX 127
 
 /////////////////////////////////////////////////////////////////////////////
 // Local Variables
 /////////////////////////////////////////////////////////////////////////////
 
+enum proc_sm State;
+int Splitpoint = SPLIT_MAX;
 
 /////////////////////////////////////////////////////////////////////////////
 // Local Prototypes
@@ -56,6 +61,29 @@ s32 PROC_Init(u32 mode)
 }
 
 /////////////////////////////////////////////////////////////////////////////
+// init
+/////////////////////////////////////////////////////////////////////////////
+s32 PROC_sm(u8 program_change)
+{
+  u8 number = program_change & 7;
+//  u8 bank = (program_change >> 3) & 7;
+//  u8 group = program_change >> 6;
+
+  if (number == 0) {
+    State = ARMED;
+    MIOS32_MIDI_SendDebugMessage("SM ARMED\n");
+  } else if (State == ARMED && number == 1) {
+    State = SPLIT_SET;
+    MIOS32_MIDI_SendDebugMessage("SM SPLIT_SET\n");
+  } else {
+    MIOS32_MIDI_SendDebugMessage("Unkown combo: ->SM IDLE\n");
+    State = IDLE;
+  }
+
+  return State;
+}
+
+/////////////////////////////////////////////////////////////////////////////
 // Handle incoming MIDI data
 /////////////////////////////////////////////////////////////////////////////
 s32 PROC_MIDI_NotifyPackage(mios32_midi_port_t port, mios32_midi_package_t midi)
@@ -66,17 +94,29 @@ MIOS32_MIDI_SendDebugMessage("Received midi from port %d\n", port);
 
   switch (midi.type) {
   case NoteOff:
+    if (midi.note > Splitpoint) midi.chn++;
     MIOS32_MIDI_SendNoteOn(port, midi.chn, midi.note, midi.velocity);
     break;
 
   case NoteOn:
+    if (State == SPLIT_SET) {
+      MIOS32_MIDI_SendDebugMessage("Set split to %d\n", midi.note);
+      Splitpoint = midi.note;
+      State = IDLE;
+      break; // Don't echo note
+    }
+
+    if (midi.note > Splitpoint) midi.chn++;
     MIOS32_MIDI_SendDebugMessage("Note on: port %d, chan %d, note %d, vel %d\n",
                                  port, midi.chn, midi.note, midi.velocity);
     MIOS32_MIDI_SendNoteOn(port, midi.chn, midi.note, midi.velocity);
+
     break;
 
   case CC:
     MIOS32_MIDI_SendCC(port, midi.chn, midi.cc_number, midi.value);
+    if (Splitpoint < SPLIT_MAX)
+      MIOS32_MIDI_SendCC(port, midi.chn + 1, midi.cc_number, midi.value);
 #if 0
     if (midi.cc_number == 120 || midi.cc_number >= 123) {
       // all notes off
@@ -92,6 +132,21 @@ MIOS32_MIDI_SendDebugMessage("Received midi from port %d\n", port);
   case PitchBend:
     bend_val = (midi.evnt2 << 7) + midi.evnt1; //  - 0x2000;
     MIOS32_MIDI_SendPitchBend(port, midi.chn, bend_val);
+    if (Splitpoint < SPLIT_MAX)
+      MIOS32_MIDI_SendPitchBend(port, midi.chn + 1, bend_val);
+    break;
+
+  case ProgramChange:
+    MIOS32_MIDI_SendDebugMessage("Program change: port %d, chan %d, value %d\n",
+                                 port, midi.chn, midi.program_change);
+    PROC_sm(midi.program_change);
+    // And don't send it on.
+    break;
+
+  case Aftertouch:
+    MIOS32_MIDI_SendAftertouch(port, midi.chn, midi.evnt1);
+    if (Splitpoint < SPLIT_MAX)
+      MIOS32_MIDI_SendAftertouch(port, midi.chn + 1, midi.evnt1);
     break;
 
   default:
